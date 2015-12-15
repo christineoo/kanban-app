0f800873b89c6c13f5807184d5402f3ca3f38055
Christine Oo <ootjinvoon@gmail.com>
2015-12-15 16:06:53 +0900

    Adding new notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,31 +2,48 @@ import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
 
-const notes = [
-  {
-      id: uuid.v4(),
-      task: 'Learn Webpack'
-  },
-  {
-      id: uuid.v4(),
-      task: 'Learn React'
-  },
-  {
-      id: uuid.v4(),
-      task: 'Do laundry'
-  },
-  {
-      id: uuid.v4(),
-      task: 'Knitting'
-  }
-];
-
 export default class App extends React.Component {
+
+  constructor(props) {
+    {/* props wont get set if you dont pass props to super */}
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Knitting'
+        }
+      ]
+    };
+  }
   render() {
+    const notes = this.state.notes;
     return (
       <div>
+        <button className="add-note" onClick={this.addNote}>+</button>
         <Notes items={notes} />
       </div>
     );
+  }
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    })
+    console.log('add note');
   }
 }
