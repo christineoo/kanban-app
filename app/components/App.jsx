c1a3b4c953ba5bb121579b738284d2e90da6d080
Christine Oo <ootjinvoon@gmail.com>
2015-12-17 11:19:42 +0900

    [Flux] adding alt, store and action and restucture code to follow flux architecture

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,32 +1,26 @@
 import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore'
 
 export default class App extends React.Component {
 
   constructor(props) {
     {/* props wont get set if you dont pass props to super */}
     super(props);
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Knitting'
-        }
-      ]
-    };
+    this.state = NoteStore.getState();
+  }
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+  componentWillMount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+  storeChanged = (state) => {
+    // Without a property initializer `this` wouldn't
+    // point at the right context (defaults to `undefined` in strict mode).
+    this.setState(state);
   }
   render() {
     const notes = this.state.notes;
@@ -38,27 +32,14 @@ export default class App extends React.Component {
     );
   }
   addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
-    })
+    NoteActions.create({task: 'New task'});
     console.log('add note');
   }
   editNote = (id, task) => {
-    const notes = this.state.notes.map((note) => {
-      if(note.id === id) {
-        note.task = task;
-      }
-      return note;
-    });
-    this.setState({notes});
+    NoteActions.update({id, task});
     console.log('note edited', id, task);
   }
   deleteNote = (id) => {
-    this.setState({
-      notes: this.state.notes.filter((note) => note.id !== id)
-    });
+    NoteActions.delete(id);
   }
 }
